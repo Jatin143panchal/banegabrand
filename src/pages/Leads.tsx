aad98b960e4212e9f59c45b75ee6d4a2d5d49d94
Jatin143panchal <148187766+Jatin143panchal@users.noreply.github.com>
2026-06-14 16:29:17 +0530

    Update Leads.tsx

--- src/pages/Leads.tsx
+++ src/pages/Leads.tsx
@@ -717,7 +717,7 @@ export default function Leads() {
         return;
       }
       
-      const response = await fetch(`${supabaseUrl}/functions/v1/esign-final`, {
+      const response = await fetch(`${supabaseUrl}/functions/v1/leegality-prod`, {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${session.access_token}`,
